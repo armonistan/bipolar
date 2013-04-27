9cef76a7137f58dc5cf01e4b7e5a690f951165d8
Phantarch <rturben@uark.edu>
2013-04-27 09:25:02 -0700

    Alpha 0.77

--- src/com/bipolar/entities/Field.java
+++ src/com/bipolar/entities/Field.java
@@ -4,7 +4,6 @@ public class Field extends Entity{
 
 	public Field(int xpos, int ypos, boolean solid, int state, int drawLayer) {
 		super(xpos, ypos, solid, state, drawLayer);
-		// TODO Auto-generated constructor stub
 	}
 
 }
